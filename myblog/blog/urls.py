af8b90ca19d05755354b4bfd41c3dc72d232f048
Denis Mtv <Freeflop@outlook.com>
2023-02-23 15:17:06 +0300

    number of likes/dislikes and checking ip address verification added

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -4,4 +4,8 @@ from . import views
 # пустые кавычки - главная стр нашего сайта
 urlpatterns = [path('', views.PostView.as_view()),
                path('<int:pk>/', views.PostDetail.as_view()),
-               path('review/<int:pk>/', views.AddComments.as_view(), name='add_comments')]
+               path('review/<int:pk>/', views.AddComments.as_view(),
+                    name='add_comments'),
+               path('<int:pk>/add_likes/',
+                    views.AddLike.as_view(), name='add_likes'),
+               path('<int:pk>/del_likes/', views.DelLike.as_view(), name='del_likes')]
